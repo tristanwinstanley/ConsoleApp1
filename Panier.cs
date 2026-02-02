b458caf6fd0123785c32fbed2c42dea4836fa256
tristanwinstanley <winstanley.tristan@gmail.com>
2026-02-02 21:54:09 +0100

    panier

---
+++ Panier.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExoHeritage
+{
+    internal class Panier
+    {
+        private List<Instrument> instruments;
+        public string NomClient
+        {
+            get
+            {
+                return NomClient.ToLower();
+            }
+            set
+            {
+                NomClient = value;
+            }
+        }
+        public Panier()
+        {
+        }
+        public void AjouterInstruments()
+        {
+            instruments.Add(new Piano());
+            instruments.Add(new Piano());
+            instruments.Add(new Guitare());
+        }
+        public int CalculerPrixDuPanier()
+        {
+            int total = 0;
+            int prixMini = 0;
+
+            foreach (Instrument instrument in instruments)
+            {
+                int prix = instrument.Prix;
+
+                total += prix;
+
+                if (prix < prixMini)
+                {
+                    prixMini = prix;
+                }
+            }
+
+            if (instruments.Count >= 3)
+            {
+                total = total - prixMini;
+            }
+
+            return total;
+        }
+    }
+}
